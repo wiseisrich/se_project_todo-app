b6e4a076899ee8e16c0fbef5dd91bf34a6dedb4d
Salomon Paul <juniorp2282@yahoo.com>
2026-03-06 19:21:07 -0500

    update .prettierignore

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopup.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
-// const todoTemplate = document.querySelector("#todo-template"); removed
 const todosList = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
@@ -19,43 +18,10 @@ const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
 };
 
-// The logic in this function should all be handled in the Todo class.
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
   return todoElement;
-
-  // const todoElement = todoTemplate.content
-  //   .querySelector(".todo")
-  //   .cloneNode(true);
-  // const todoNameEl = todoElement.querySelector(".todo__name");
-  // const todoCheckboxEl = todoElement.querySelector(".todo__completed");
-  // const todoLabel = todoElement.querySelector(".todo__label");
-  // const todoDate = todoElement.querySelector(".todo__date");
-  // const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
-
-  // todoNameEl.textContent = data.name;
-  // todoCheckboxEl.checked = data.completed;
-
-  // // Apply id and for attributes.
-  // // The id will initially be undefined for new todos.
-  // todoCheckboxEl.id = `todo-${data.id}`;
-  // todoLabel.setAttribute("for", `todo-${data.id}`);
-
-  // // If a due date has been set, parsing this it with `new Date` will return a
-  // // number. If so, we display a string version of the due date in the todo.
-  // const dueDate = new Date(data.date);
-  // if (!isNaN(dueDate)) {
-  //   todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
-  //     year: "numeric",
-  //     month: "short",
-  //     day: "numeric",
-  //   })}`;
-  // }
-
-  // todoDeleteBtn.addEventListener("click", () => {
-  //   todoElement.remove();
-  // });
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -71,7 +37,6 @@ addTodoForm.addEventListener("submit", (evt) => {
   const name = evt.target.name.value;
   const dateInput = evt.target.date.value;
 
-  // Create a date object and adjust for timezone
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
 
